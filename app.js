4e4ceabdfebfcf75d4d1a2fba4e29b5e68350e8e
00yo000yo <00yo000yo@gmail.com>
2024-03-12 22:02:26 +0800

    update 20210312

--- app.js
+++ app.js
@@ -1,64 +1,59 @@
 const express = require("express")
 const app = express()
 const port = 3000
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 const db = mongoose.connection
-const exhbs = require('express-handlebars')
-const bodyParser = require('body-parser')
+const exhbs = require("express-handlebars")
+const bodyParser = require("body-parser")
 const Urls = require("./models/urls")
-const random = require('./shortRandom')
+const random = require("./shortRandom")
 
-const findOrCreate = require('mongoose-findorcreate')
-const shortRandom = require("./shortRandom")
-
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
 }
-mongoose.connect(process.env.MONGODB_URI,
-    { useNewUrlParser: true ,useUnifiedTopology: true})
-
-db.once('open',()=>{
-    console.log('connected!')
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
 
-app.engine('hbs',exhbs({defaultLayout: 'main', extname: '.hbs'}))
-app.set('view engine', 'hbs')
+db.once("open", () => {
+  console.log("connected!")
+})
+
+app.engine("hbs", exhbs({ defaultLayout: "main", extname: ".hbs" }))
+app.set("view engine", "hbs")
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/',(req,res)=>{
-    res.render('index')
+app.get("/", (req, res) => {
+  res.render("index")
 })
 
-app.post('/', (req,res) => {
-    const originalUrl = req.body.url
+app.post("/", (req, res) => {
+  const originalUrl = req.body.url
+  const short = random(5)
 
-    Urls.findOne({url : originalUrl} , function (err , result) {
-        if(err){console.log(error)
-
-        return}
-
-        if(result){
-            res.render('finish',{ data:result.shortRandom })
-        }else{
-            const short = random(5)
-            Urls.create({
-            url : originalUrl,
-            shortRandom : short, 
+  Urls.findOne({ url: originalUrl })
+    .then((result) => {
+      if (!result) {
+        return Urls.create({
+          url: originalUrl,
+          shortRandom: short,
         })
-        .then(() => res.render('finish', { data:short }))
-
-    }})
+      } else {
+        return result
+      }
+    })
+    .then((result) => res.render("finish", { data: result.shortRandom }))
 })
 
-app.get('/:short',(req,res)=>{
-    const {short} = req.params
-    console.log(short)
-    return Urls.findOne({ shortRandom: short})
+app.get("/:short", (req, res, next) => {
+  const { short } = req.params
+  return Urls.findOne({ shortRandom: short })
     .lean()
-    .then((url) => res.render('finish', { originalUrl:url }))
+    .then((url) => res.render("finish", { originalUrl: url }))
+    .catch(next)
 })
 
-
-app.listen(port,()=>{
-    console.log('gogo')
+app.listen(port, () => {
+  console.log("gogo")
 })
